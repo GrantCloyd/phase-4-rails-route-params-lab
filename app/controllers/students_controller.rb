5e186fe71fe699b0307acbd0c56b4511adbeb1ad
Grant Cloyd <dgcloyd@gmail.com>
2021-07-18 20:33:31 -0500

    commit tree 35a76804be041f81d33d9b171c81283a7ebf9200

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,22 @@
 class StudentsController < ApplicationController
 
   def index
+    if params[:name]
+    
+      render json: Student.find_by(first_name: params[:name])
+        
+  else
     students = Student.all
     render json: students
+  end
+  end
+
+  def get_by_id 
+   render json: Student.find(params[:id])
+  end
+
+  def get_by_name
+   byebug
   end
 
 end
